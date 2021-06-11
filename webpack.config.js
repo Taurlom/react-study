a25e68f0f4b06b6c4da5afbf9e2505b3d2b53444
m.makarenkov <m.makarenkov@tinkoff.ru>
2021-06-11 21:44:41 +0300

    Add webpack dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -7,6 +8,11 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ],
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
