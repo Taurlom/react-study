6c3e718e994aea540876eee5e45620c033b0c777
m.makarenkov <m.makarenkov@tinkoff.ru>
2021-06-14 22:18:28 +0300

    Add HMR

--- cfg/webpack.client.config.js
+++ cfg/webpack.client.config.js
@@ -1,17 +1,26 @@
 const path = require('path');
+const { HotModuleReplacementPlugin } = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV;
 const IS_DEV = NODE_ENV === 'development';
 
 module.exports = {
     mode: NODE_ENV ? NODE_ENV : 'development',
-    entry: './src/client/index.jsx',
+    entry: [
+        path.resolve(__dirname, '../src/client/index.jsx'),
+        'webpack-hot-middleware/client?path=http://localhost:3001/static/__webpack_hmr'
+    ],
     output: {
         path: path.resolve(__dirname, '../dist/client'),
-        filename: 'client.js'
+        filename: 'client.js',
+        publicPath: '/static/'
     },
     resolve: {
-        extensions: [".jsx", ".js", "json"]
+        extensions: [".jsx", ".js", "json"],
+        alias: {
+            'react-dom': IS_DEV ? '@hot-loader/react-dom' : 'react-dom'
+        }
     },
     module: {
         rules: [
@@ -20,5 +29,11 @@ module.exports = {
                 use: ['ts-loader']
             }
         ]
-    }
+    },
+    plugins: IS_DEV
+        ? [
+            new CleanWebpackPlugin(),
+            new HotModuleReplacementPlugin()
+        ]
+        : [],
 };
--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { hot } from 'react-hot-loader/root';
 
-const Header = () => (
-    <header><h1>Hello from Headerr component</h1></header>
+const HeaderComponent = () => (
+    <header>
+        <h1>Hello from Headerr component</h1>
+    </header>
 );
+
+const Header = hot(HeaderComponent);
 
 export default Header;
